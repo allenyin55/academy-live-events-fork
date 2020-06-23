0721abc2e318245cc9007be85c606ddd2df7ec20
Dean Wampler <dean@concurrentthought.com>
2020-06-23 12:43:19 -0700

    Now only asks for session data if the session is truly ACTIVE

--- scripts/get-sessions.sh
+++ scripts/get-sessions.sh
@@ -5,7 +5,7 @@ dir=$(dirname $0)
 . $dir/utils.sh
 
 script_name=$0
-tagline="Write the session names, session ids, head-node IP addresses, juypter tokens, and URLs."
+tagline="Write the session names, session ids, head-node IP addresses, juypter tokens, and URLs for the specified range of sessions, but only if they are ACTIVE."
 cmd_opts=(session_name range)
 post_help_messages=slow_warning
 
@@ -34,16 +34,10 @@ do
 	shift
 done
 
-[[ -z $NOOP ]] || error "Can't process the NOOP = $NOOP flag any further"
-
-sessions=()
-start=
-anyscale list sessions 2> /dev/null | while read line
+declare -A sessions
+$NOOP anyscale list sessions 2> /dev/null | grep ACTIVE | cut -f1 -d' ' | while read session
 do
-	[[ $line =~ SESSION ]] && start=true && continue
-	[[ $start ]] || continue
-	session=$(echo $line | sed -e 's/^\([^ ]*\).*/\1/')
-	sessions+=("$session")
+	sessions[$session]=true
 done
 
 # The "assigned email" field is blank. It's here for convenience when importing the
@@ -54,28 +48,33 @@ echo "SESSION,ID,DASHBOARD_IP,JUPYTER_TOKEN,JUPYTER_URL,DASHBOARD_URL,TENSORBOAR
 [[ ${#range[@]} -eq 0 ]] && range=($M $N)
 compute_range ${range[@]} | while read M N M0 N0 MN0
 do
-	echo "Getting session data for sessions numbered N = $M0 to $N0 with name format ${name_prefix}-N:" >&2
+	echo "Getting session data for sessions numbered N = $M0 to $N0 with name format ${name_prefix}-N:" 1>&2
 
 	for n in {$M..$N}
 	do
 		n0=$(zero_pad $n)
 		npn=${name_prefix}-${n0}
-		if [[ -z $NOOP ]]
+		if [[ -z $sessions[$npn] ]]
 		then
-			ip=$(anyscale ray get-head-ip $npn 2> /dev/null)
-			anyscale ray exec-cmd "$npn" '~/anaconda3/bin/jupyter notebook list' | \
-				grep '^http' | sed -e 's?.*sessions/\([0-9]*\)/.*token=\([^ ]*\).*?\1 \2?' | \
-				while read id token
-				do
-					durl="http://$ip:8081/auth/?token=$token&redirect_to=dashboard"
-					jurl="https://anyscale.dev/sessions/$id/jupyter/lab?token=$token"
-					turl="https://anyscale.dev/sessions/$id/auth/?token=$token&session_id=$id&redirect_to=tensorboard"
-
-					echo "$npn,$id,$ip,$token,$jurl,$durl,$turl,"
-				done
+			echo "WARN: Session $npn wasn't found in 'anyscale list sessions'. Skipping..." 1>&2
 		else
-			$NOOP anyscale ray get-head-ip $npn
-			$NOOP anyscale ray exec-cmd "$npn" '~/anaconda3/bin/jupyter notebook list'
+			if [[ -z $NOOP ]]
+			then
+				ip=$(anyscale ray get-head-ip $npn 2> /dev/null)
+				anyscale ray exec-cmd "$npn" '~/anaconda3/bin/jupyter notebook list' | \
+					grep '^http' | sed -e 's?.*sessions/\([0-9]*\)/.*token=\([^ ]*\).*?\1 \2?' | \
+					while read id token
+					do
+						durl="http://$ip:8081/auth/?token=$token&redirect_to=dashboard"
+						jurl="https://anyscale.dev/sessions/$id/jupyter/lab?token=$token"
+						turl="https://anyscale.dev/sessions/$id/auth/?token=$token&session_id=$id&redirect_to=tensorboard"
+
+						echo "$npn,$id,$ip,$token,$jurl,$durl,$turl,"
+					done
+			else
+				$NOOP anyscale ray get-head-ip $npn
+				$NOOP anyscale ray exec-cmd "$npn" '~/anaconda3/bin/jupyter notebook list'
+			fi
 		fi
 	done
 done
